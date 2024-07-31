25ce11e08d40cf77f80ae0b1194711f21638929a
Brennan Vincent <brennan@umanwizard.com>
2024-07-31 15:29:51 +0100

    Get Go custom labels in unwinder
    This adds the eBPF code for getting Go custom labels. It is dead without
    without the user-mode Go code, in a future commit.

--- support/ebpf/bpfdefs.h
+++ support/ebpf/bpfdefs.h
@@ -92,6 +92,9 @@ static long (*bpf_probe_read_user)(void *dst, int size, const void *unsafe_ptr) 
     (void *)BPF_FUNC_probe_read_user;
 static long (*bpf_probe_read_kernel)(void *dst, int size, const void *unsafe_ptr) =
     (void *)BPF_FUNC_probe_read_kernel;
+struct bpf_perf_event_data;
+static long (*bpf_perf_prog_read_value)(struct pt_regs *ctx, struct bpf_perf_event_value *buf, u32 buf_size) =
+    (void *)BPF_FUNC_perf_prog_read_value;
 
 // The sizeof in bpf_trace_printk() must include \0, else no output
 // is generated. The \n is not needed on 5.8+ kernels, but definitely on
@@ -144,5 +147,15 @@ static long (*bpf_probe_read_kernel)(void *dst, int size, const void *unsafe_ptr
   _Pragma("GCC diagnostic pop")
 
 #endif // !TESTING_COREDUMP
+
+// HACK: On failure, bpf_perf_prog_read_value() zeroes the buffer. We ensure that this always
+// fail with a compile time assert that ensures that the struct size is different to the size
+// of the expected structure.
+#define bpf_large_memzero(_d, _l)                                                                                        \
+    ({                                                                                                                   \
+        _Static_assert(_l != sizeof(struct bpf_perf_event_value), "stack size must be different to the valid argument"); \
+        bpf_perf_prog_read_value(ctx, _d, _l);                                                                           \
+    })
+
 
 #endif // OPTI_BPFDEFS_H
